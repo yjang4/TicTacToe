37304679b95fdd65bc5e58fba2f5959ada5ee4ca
Daniel <mikyjo2012@gmail.com>
2023-05-25 22:55:54 -0500

    error on click

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const GameBoard = (() => {
-    const board = [0,0,0,0,0,0,0,0,0];
+    const board = ['x','','','','','','','',''];
+    function boardClick(index) {
+        board[index] = 'X';
+        console.log("da");
+        DisplayController.updateBoard();
+    }
 
     return {
         board,
-
+        boardClick
     };
 })();
 const DisplayController = (() => {
@@ -13,13 +18,23 @@ const DisplayController = (() => {
             const div = document.createElement('div');
             div.classList.add('unit');
             div.setAttribute('id', i);
+            div.addEventListener("onclick", GameBoard.boardClick(i));;
             div.textContent= GameBoard.board[i];
             boardDiv.appendChild(div);
         }
     }
+    function removeBoard() {
+        while (boardDiv.firstChild) {
+            boardDiv.removeChild(boardDiv.firstChild);
+        }
+    }
+    function updateBoard() {
+        removeBoard();
+ 
+    }
     return {
         displayBoard,
-   
+        updateBoard,
     };
 })();
 DisplayController.displayBoard();
